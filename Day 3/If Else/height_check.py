99e78eeb8f3cdce846d9a5651a8ed3aa3dae38a4
Ron Diggs <rodiggity@google.com>
2025-01-22 13:27:24 -0800

    Use if and else statement to check if someone is tall enough to ride a rollercoaster

---
+++ Day 3/If Else/height_check.py
@@ -0,0 +1,7 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+if height >= 120:
+    print("You are able to ride this rollercoaster")
+else:
+    print("You are too short for this ride")
