161fca35ed58c29c5eb82a26584125f171da05c2
Ron Diggs <rodiggity@google.com>
2025-01-23 14:39:12 -0800

    Created coin flip using random class

---
+++ Day 4/Random Module/heads_or_tails.py
@@ -0,0 +1,16 @@
+import random
+
+#rand_num = random.randint(1, 10)
+#print(rand_num)
+
+#random_num_0_to_1 = random.random() * 10
+#print(random_num_0_to_1)
+
+#ran_float = random.uniform(1,10)
+#print(ran_float)
+
+heads_or_tails = random.randint(1,2)
+if heads_or_tails == 1:
+    print("Tails")
+else:
+    print("Heads")
