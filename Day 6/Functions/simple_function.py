54b9d0c86435d97a06dc5cbda0388a1fd141b253
Ron Diggs <Rj350t@gmail.com>
2025-01-25 18:09:29 -0800

    Created a simple function to print out Hello by Lionel Richie

---
+++ Day 6/Functions/simple_function.py
@@ -0,0 +1,8 @@
+def my_function():
+    print("Hello, is it me you're looking for?")
+    print("I can see it in your eyes, I can see it in your smile")
+    print("You're all I've ever wanted, and my arms are open wide")
+    print("'Cause you know just what to say, and you know just what to do")
+    print("And I want to tell you so much, I love you")
+
+my_function()
