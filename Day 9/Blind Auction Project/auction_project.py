d512630f6fae4ed3a627ce131ec7fcee23ae6a48
Ron Diggs <Rj350t@gmail.com>
2025-01-29 20:40:44 -0800

    Created an auction project, storing variables in dictionaries, fixed comparison issue in function

--- Day 9/Blind Auction Project/auction_project.py
+++ Day 9/Blind Auction Project/auction_project.py
@@ -1,26 +1,25 @@
-import art
-from os import system, name
-print(art.logo)
+import art  #import ascii art
+from os import system, name #import system and name from OS
+print(art.logo) #print ascii art
 
-other_bidders = True
-bids = {}
+other_bidders = True #set other bidders to true for while loop
+bids = {} #dfine bids dictionary
 
-def clear():
+def clear(): #define function to clear screen depending on OS
     if name == 'nt':
         _ = system('cls')
     else:
         _ = system ('clear')
 
-def fun():
-    uname = input("What is your name?: ")
+def fun(): #define main function
+    uname = input("What is your name?: ") #get user input
     ubid = int(input("What is your bid?: $"))
-    bids[uname] = {ubid}
-    question = input("Are there any other bidders? Type 'yes' or 'no'.\n").lower()
-    if question == "yes":
+    bids[uname] = ubid #store name and bid within dictionary
+    question = input("Are there any other bidders? Type 'yes' or 'no'.\n").lower() #ask question to find more bidders
+    if question == "yes": #if more bidders clear screen and loop
         clear()
         return True
-    else:
-        print(bids)
+    else: #if false get highest bidder then present winner
         winner = ""
         highest_bid = 0
         for bidder in bids:
@@ -30,7 +29,7 @@ def fun():
                 winner = bidder
         return False
 
-while other_bidders:
+while other_bidders: #loop through function
     other_bidders = fun()
 
 
